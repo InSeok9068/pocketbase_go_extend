8c193e7c09639573478f9b988ab670f5ac4ea85e
Lee In Seok <dlstjr9068@gmail.com>
2024-06-27 09:43:09 +0900

    실행 시 트리거 메서드 호출

--- cmd/playground.go
+++ cmd/playground.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"log"
-
+	"fmt"
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+	"log"
 )
 
 func main() {
 	app := pocketbase.New()
+
+	app.OnAfterBootstrap().Add(func(e *core.BootstrapEvent) error {
+		user, err := app.Dao().FindAuthRecordByEmail("users", "dlstjr9068@gmail.com")
+		fmt.Print(user)
+		fmt.Print(err)
+		return nil
+	})
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
