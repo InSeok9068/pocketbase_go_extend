d7a3660d32abc6f840ec62e2abc0081f44975ba7
Lee In Seok <dlstjr9068@gmail.com>
2024-07-01 14:11:28 +0900

    api 샘플 테스트

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
@@ -15,6 +17,11 @@ func main() {
 	// serves static files from the provided public dir (if exists)
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		e.Router.GET("/hello/:name", func(c echo.Context) error {
+			name := c.PathParam("name")
+			return c.JSON(http.StatusOK, map[string]string{"message": "Hello " + name})
+		})
+
 		return nil
 	})
 
